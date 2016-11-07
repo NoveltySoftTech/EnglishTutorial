743ed1265373405272788e25063ff13bd470e3d6
mahikasarla <mahi.kasarla09@gmail.com>
2016-11-06 19:43:29 -0500

    Added verb_tenses Table

---
+++ src/main/resources/db/migration/V2__verbTenses.sql
@@ -0,0 +1,7 @@
+create table verb_tenses(
+id int auto_increment PRIMARY KEY,
+present VARCHAR(50) NOT NULL,
+past VARCHAR(50) NOT NULL,
+past_participle VARCHAR(50) NOT NULL,
+continues VARCHAR(50) NOT NULL
+)
